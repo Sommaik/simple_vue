8e05607ef71c57bbfbdcf78e3b620dede7bfd8b2
SOMMAI KRANGPANICH <sommai.k@gmail.com>
2019-07-18 15:40:14 +0700

    add page

--- src/router.js
+++ src/router.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
+import Admin from '@/views/admin/Admin.vue'
+import Setup from '@/views/admin/Setup.vue'
+import Profile from '@/views/admin/Profile.vue'
 
 Vue.use(Router)
 
@@ -20,6 +24,11 @@ export default new Router({
       component: Login
     },
     {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
@@ -27,6 +36,23 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: Admin,
+      children: [
+        {
+          path: 'setup',
+          name: 'setup',
+          component: Setup
+        },
+        {
+          path: 'profile',
+          name: 'profile',
+          component: Profile
+        }
+      ]
     }
   ]
 })
